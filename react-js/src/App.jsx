d3c272f89fb8562aac7086f30533ff144e2f19ac
Rahul <123063375+RahulCSS@users.noreply.github.com>
2025-06-02 22:10:40 +0530

    Tailwind styling configuration

--- react-js/src/App.jsx
+++ react-js/src/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import { Routes, Route } from "react-router";
+import Counter from './pages/Counter';
 
 const App = () => {
   return (
     <div>
       <Routes>
-      <Route path="/" element={<App />} />
+      <Route path="/" element={<Counter />} />
       </Routes>
     </div>
   )
---
+++ react-js/src/pages/Counter.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Counter = () => {
+  return (
+    <div class="text-3xl font-bold underline">Counter</div>
+  )
+}
+
+export default Counter
