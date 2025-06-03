097c4cfcb716b4b910d3e3953ecc347b24397619
Rahul <123063375+RahulCSS@users.noreply.github.com>
2025-06-04 00:23:33 +0530

    Router App

--- react-js/src/App.jsx
+++ react-js/src/App.jsx
@@ -1,24 +1,27 @@
 import React from 'react'
 import { Routes, Route } from "react-router";
-import Counter from './pages/Counter';
+import Counter from './pages/counterApp/Counter';
 import Home from './pages/Home';
-import TodoList from './pages/TodoList';
-import Landing from './pages/routerPages/Landing';
-import About from './pages/routerPages/About';
-import Contact from './pages/routerPages/Contact';
-import WeatherAPI from './pages/WeatherAPI';
+import TodoList from './pages/TodoApp/TodoList';
+import Landing from './pages/routerApp/Landing';
+import About from './pages/routerApp/About';
+import Contact from './pages/routerApp/Contact';
+import WeatherAPI from './pages/weatherApp/WeatherAPI';
+import RouterApp from './pages/routerApp/RouterApp';
 
 const App = () => {
   return (
     <div className ="box-border m-[0px] p-[0px] h-screen">
       <Routes>
         <Route index element={<Home />} />
-        <Route path="/counter" element={<Counter />} />
-        <Route path="/tasks" element={<TodoList />} />
-        <Route path="/home" element={<Landing />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/weather" element={<WeatherAPI />} />
+        <Route path="counter" element={<Counter />} />
+        <Route path="todo" element={<TodoList />} />
+        <Route path="router" element={<RouterApp />} >
+          <Route path="home" element={<Landing />} />
+          <Route path="about" element={<About />} />
+          <Route path="contact" element={<Contact />} />
+        </Route >
+        <Route path="weather" element={<WeatherAPI />} />
       </Routes>
     </div>
   )
--- react-js/src/pages/routerApp/RouterApp.jsx
+++ react-js/src/pages/routerApp/RouterApp.jsx
@@ -1,30 +1,35 @@
 import React from 'react'
 import { useNavigate } from 'react-router';
+import { Outlet } from 'react-router';
 
-const Home = () => {
+const RouterApp = () => {
   const navigate = useNavigate();
-  const menu = [
-    { label: "Counter", route: "/counter" },
-    { label: "TodoList", route: "/todo" },
-    { label: "Router", route: "/router" },
-    { label: "Weather", route: "/weather" },
+  const routermenu = [
+    { label: "Home", route: "home" },
+    { label: "About", route: "about" },
+    { label: "Contact", route: "contact" },
   ];
 
   return (
     <div className="flex flex-col justify-center items-center min-h-screen bg-gray-100">
-        <div className="flex flex-col justify-center items-center gap-[1rem] h-[200px] w-[400px]">
-            {menu.map((item) => (
+        <div className="flex justify-center items-center gap-[1rem] h-[200px] w-[400px]">
+            {routermenu.map((item) => (
           <div
             key={item.label}
-            onClick={() => navigate(item.route)}
+            onClick={() => navigate(`/router/${item.route}`)}
             className="w-full text-center py-3 rounded border border-transparent hover:border-gray-300 hover:shadow-md transition duration-200 cursor-pointer bg-white text-[1.5rem]"
           >
             {item.label}
           </div>
         ))}
         </div>
+
+        <div className="px-4 text-center">
+        <Outlet />
+        </div>
+        
     </div>
   )
 }
 
-export default Home
+export default RouterApp
