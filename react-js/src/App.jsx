ba501b3107f3fe6a06d7e83612b3edf0ceddef9a
Rahul <123063375+RahulCSS@users.noreply.github.com>
2025-06-03 22:54:41 +0530

    Router Navigation

--- react-js/src/App.jsx
+++ react-js/src/App.jsx
@@ -2,13 +2,23 @@ import React from 'react'
 import { Routes, Route } from "react-router";
 import Counter from './pages/Counter';
 import Home from './pages/Home';
+import TodoList from './pages/TodoList';
+import Landing from './pages/routerPages/Landing';
+import About from './pages/routerPages/About';
+import Contact from './pages/routerPages/Contact';
+import WeatherAPI from './pages/WeatherAPI';
 
 const App = () => {
   return (
-    <div class ="box-border m-[0px] p-[0px] h-screen">
+    <div className ="box-border m-[0px] p-[0px] h-screen">
       <Routes>
         <Route index element={<Home />} />
         <Route path="/counter" element={<Counter />} />
+        <Route path="/tasks" element={<TodoList />} />
+        <Route path="/home" element={<Landing />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/weather" element={<WeatherAPI />} />
       </Routes>
     </div>
   )
--- react-js/src/pages/Counter.jsx
+++ react-js/src/pages/Counter.jsx
@@ -17,12 +17,12 @@ const Counter = () => {
 
 
   return (
-    <div class="flex flex-col justify-center items-center min-h-screen">
-      <div class="text-2xl sm:text-3xl md:text-4xl lg:text-5xl font-bold text-center text-blue-600">Counter App</div>
-      <div class="flex flex-col justify-center items-center gap-[1rem] h-[200px] w-[400px]">
-        <div class="flex gap-[1rem]">
+    <div className="flex flex-col justify-center items-center min-h-screen">
+      <div className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl font-bold text-center text-blue-600">Counter App</div>
+      <div className="flex flex-col justify-center items-center gap-[1rem] h-[200px] w-[400px]">
+        <div className="flex gap-[1rem]">
           <button onClick={() => handleCounter('decrement')}  class="text-[1.5rem] px-[1rem] bg-gray-100 border border-gray-300 rounded-md">-</button>
-          <span class="text-[1.5rem]">{count}</span>
+          <span className="text-[1.5rem]">{count}</span>
           <button onClick={() => handleCounter('increment')} class="text-[1.5rem] px-[1rem] bg-gray-100 border border-gray-300 rounded-md">+</button>
         </div>
         <button onClick = {resetCounter} class="text-[1.5rem] px-[1rem] bg-gray-100 border border-gray-300 rounded-md">Reset</button>
